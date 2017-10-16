57710dedb40caf89d334346b9bdfc76838f0942f
magendreau <32451540+magendreau@users.noreply.github.com>
2017-10-16 22:29:29 +0200

    Add files via upload

--- Neuron.cpp
+++ Neuron.cpp
@@ -1,10 +1,11 @@
 #include "Neuron.hpp"
 #include <iostream>
 #include <cmath>
+#include <cassert>
 
 using namespace std;
 
-Neuron::Neuron(double potential, double spike, double t, State st, std::vector<unsigned int> buffer)
+Neuron::Neuron(double potential, unsigned int spike, double t, State st, vector<unsigned int> buffer)
 	:membranePotential(potential), spikes(spike), spikesOccured(t), state(st), ringBuffer(buffer)
 {}
 	
@@ -13,7 +14,7 @@ double Neuron::getMembranePotential() const
 	return membranePotential;
 }
 
-double Neuron::getSpikes() const
+unsigned int Neuron::getSpikes() const
 {
 	return spikes;
 }
@@ -33,7 +34,7 @@ void Neuron::setMembranePotential(double potential)
 	membranePotential = potential;
 }
 
-void Neuron::setSpikes(double spk)
+void Neuron::setSpikes(unsigned int spk)
 {
 	spikes = spk;
 }
@@ -57,101 +58,60 @@ void Neuron::updateState(double t)
 	}
 }
 
-void Neuron::update(double step, double I)
+void Neuron::update(unsigned int step, double I, bool recep)
 {	
-	while(step < n_stop) { //for the local time, if it hasn't reach the total time it continues
-		updateState(step*h);
-		if(state == REFRACTORY) {
-			setMembranePotential(0.0); // when the neuron is refractory it's membrane potential is 0
-		} else if((membranePotential > Vr) and (state == NON_REFRACTORY)) { 
-			if((membranePotential >= theta) and (I > 1.0)) { //if the membrane potential reaches the thresold
-				spikesOccured = step*h; //a spike is emitted
-				cout << "A spike occured at t=" << spikesOccured << " ms" << endl;
-				++spikes;
-			}
-		}
-		setMembranePotential(newMembranePotential(I)); //calculates the new membrane potential
-		++step;
-	}
-	
-	/*while(step < n_stop) { //while the number of steps doesn't reach the total number
 		updateState(step*h);
 		
 		if(state == REFRACTORY) {
-			setMembranePotential(0.0);
-			spikes = 0.0;
-		} else if((membranePotential > Vr) and (state == NON_REFRACTORY)) {
-			//setMembranePotential(newMembranePotential(I,S));
-			if((membranePotential >= theta) and (I > 1.0)) {
-				spikesOccured = step*h;
+			membranePotential = 0.0; // when the neuron is refractory it's membrane potential is 0
+			
+		} else if((membranePotential >= theta)) { //if the membrane potential reaches the thresold
+			if(!recep) {
+				spikesOccured = step*h; //a spike is emitted
 				cout << "A spike occured at t=" << spikesOccured << " ms" << endl;
 				++spikes;
-				cout << " Local cout spikes " << spikes << endl;
-				membranePotential = 0.0;
+				state = REFRACTORY;
 			}
-		}
-		setMembranePotential(newMembranePotential(I,S));
-		++step;
-	}*/
-	
-	/*while(step < n_stop) { //while the number of steps doesn't reach the total number
-		updateState(step*h);
-
-		if((membranePotential > theta) and (I > 1.0)) {
-			spikesOccured = step*h;
-			cout << "A spike occured at t=" << spikesOccured << " ms" << endl;
-			++spikes;
-			membranePotential = 0.0;
-			state = REFRACTORY;
-		} else if(state == REFRACTORY) {
-			membranePotential = 0.0;
 		} else {
-			setMembranePotential(newMembranePotential(I,spikes));
-		}
-		++step;
-	}*/	
+			const int t = step%(D+1);
+			membranePotential = newMembranePotential(I, ringBuffer[t]); //membrane potential of neuron changes after reception
+			ringBuffer[t] = 0.0;
+	}
 }
 
-void Neuron::fillRingBuffer(double nbSpikes, unsigned int readOut, unsigned int step)
+void Neuron::fillRingBuffer(unsigned int step)
 {
-	if(step >= ringBuffer.size()) { step = 0; } //if the step of the simulation is higher than the size of the buffer 
-												//it means we can empty or replace the first case, so step goes to 0
-	ringBuffer[step*h] = nbSpikes; //stores for each time or step the number of spike emitted by the fire neuron in a place in the ring
+	const unsigned int t = (step+D)%(D+1);
+	if (spikes > 0) {
+		ringBuffer[t] += J;
+		assert((t) <= ringBuffer.size()); 
+	}
 }
 
-bool Neuron::fullBuffer(unsigned int origin)
+double Neuron::newMembranePotential(double I, double J)
 {
-	if(origin == (ringBuffer.size()+1)) { //if the origin of the ring buffer reaches the size of the buffer
-										  //the buffer is full and the delay is passed for the spikes in the first case
-		return true;
-	} 
-	return false;
+	return (membranePotential = c1*membranePotential + I*c2 +J);
 }
 
-void Neuron::receive(double step, double J, double I)
-{
-	setMembranePotential(newMembranePotential(I) + J); //membrane potential of postsynaptic neuron changes after reception and delay
-	spikesOccured = step*h; //a spike occures for the postsynaptic neuron
-	testDifferencePotential(membranePotential); //we assure that the difference between the potential is the amplitude J
-}
-
-double Neuron::newMembranePotential(double I)
-{
-	return (membranePotential = exp(-h/tau)*membranePotential + I*R*(1-exp(-h/tau)));
-}
-
-void Neuron::testDifferencePotential(double V1)
-{
-	vector <double> V(1,0.0); //vector to store the different membrane potentials
-	V.push_back(V1);
-	
-	for(size_t i(0) ; i < V.size() ; ++i) {
-		if(abs(abs(V[i]-V[i+1]) - J) < pow(10,-3)) { //if the difference between the two values and J is low enough
+void Neuron::testDifferencePotential(vector<double> V, size_t ix)
+{	
+		if(abs(abs(V[ix]-V[ix-1]) - J) <= pow(10,-3)) { //if the difference between the two values and J is low enough
 			cout << "The difference between the two potential is J" << endl;
 		} else {
+			cout << V[ix] << " and " << V[ix-1] << endl;
+			cout << abs(abs(V[ix]-V[ix-1]) - J) << endl;
 			cerr << "The difference is not J" << endl;
 		}
-	}
+}
+
+void Neuron::testDelay(vector<double> T, size_t ix)
+{
+			if(abs(abs(T[ix]-T[ix-1]) - D) <= pow(10,-3)) { //if the difference between the two values and D is low enough
+			cout << "The time between the reception and the response is D" << endl;
+		} else {
+			cout << abs(abs(T[ix]-T[ix-1]) - D) << endl;
+			cerr << "The difference is not D" << endl;
+		}
 }
 
 Neuron::~Neuron()
--- Neuron.hpp
+++ Neuron.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 constexpr double taurp(2.0); //constant of time of the repository period
 constexpr double tau(20.0); //constant of time 
@@ -12,50 +13,51 @@ constexpr double R(tau/C); //input resistance
 constexpr double h(0.1); //increment of time
 constexpr double theta(20.0); //thresold limit
 
-constexpr double Vr(10.0); //reset membrane potential
+//constexpr double Vr(0.0); //reset membrane potential
 constexpr double t_stop(500.0); //number of steps of simulation, as h=0.1, the time of simulation is here 500 ms (t=n*h)
 constexpr unsigned int n_stop(t_stop/h); //Maximal number of steps of the simulation based on time
-constexpr double D(1.5); //delay before the spike is treated by the neuron
-constexpr double J(2.0); //amplitude of the spike, equal for all synapses
+constexpr int D(15); //delay before the spike is treated by the neuron
+constexpr double J(0.1); //amplitude of the spike, equal for all synapses
+
+constexpr double c1(exp(-h/tau));
+constexpr double c2(R*(1-exp(-h/tau)));
 
 enum State {REFRACTORY, NON_REFRACTORY}; // states in which the neuron can be
 
 class Neuron {
 	
 	public:
-	Neuron( double potential = 10, double spike = 0.0, 
+	Neuron( double potential = 0.0, unsigned int spike = 0, 
 			double t = 0.0, State st = REFRACTORY, 
-			std::vector<unsigned int> buffer = std::vector<unsigned int>((D/h)));
+			std::vector<unsigned int> buffer = std::vector<unsigned int>((D/h+1)));
 			//we initialize the ring buffer at a size that matches the delay
 	
 	double getMembranePotential() const;
-	double getSpikes() const;
+	unsigned int getSpikes() const;
 	double getSpikesOccured() const;
 	State getState() const;
 	
 	void setMembranePotential(double potential);
-	void setSpikes(double spk);
+	void setSpikes(unsigned int spk);
 	void setSpikesOccured(double t);
 	void setState(State st);
 	
 	void updateState(double t); //updates the state of the neuron
-	void update(double step, double I); 
-	void fillRingBuffer(double nbSpikes, unsigned int readOut, unsigned int step);
-	bool fullBuffer(unsigned int origin); //if the ring buffer is full, it returns true
-	void receive(double step, double J, double I); //reception of spike by postsynaptic neuron
-	double newMembranePotential(double I); //recalculates the membrane potential at time t+h
+	void update(unsigned int step, double I, bool recep); 
+	void fillRingBuffer(unsigned int step);
+	double newMembranePotential(double I, double J); //recalculates the membrane potential at time t+h
 	
-	void testDifferencePotential(double V1); //verifies that the membrane potential increases of J each time
+	void testDifferencePotential(std::vector<double> V, size_t ix); //verifies that the membrane potential increases of J each time
+	void testDelay(std::vector<double> T, size_t ix);
 	
 	~Neuron();
 	
 	private:
 	double membranePotential; 
-	double spikes; //number of spikes
+	unsigned int spikes; //number of spikes
 	double spikesOccured; //time when the spikes occured
 	State state;
 	std::vector<unsigned int> ringBuffer; //buffer to keep in memory the spikes before the reaction of postsynaptic neuron
-	 
 };
 
 #endif
